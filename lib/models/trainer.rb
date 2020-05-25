2eb948c280235f8f398c636338182ff0b315483d
github username <github email address>
2020-05-25 18:21:59 -0400

    Work on trainer associations

--- lib/models/trainer.rb
+++ lib/models/trainer.rb
@@ -2,7 +2,6 @@ class Trainer < ActiveRecord::Base
   has_many :battles
   has_many :pokemons, through: :battles
 
-
   # ========================================================================
   # Trainer's instance
   # Trainer.first
