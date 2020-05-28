d420684bfd986d26f5cf1b8d72806ec32170b841
github username <github email address>
2020-05-27 21:58:59 -0400

    Add Interactivity class to clean up Interface class

---
+++ lib/models/interactivity.rb
@@ -0,0 +1,57 @@
+class Interactivity
+
+  @@legendary = ["Moltres", "Zaptos", "Articuno"]
+  @@walking_in_town = ["Walking in town", "is that a pokemon?", "No Pokemons here...", "grrr... I'm hungry! ", "WoW... was that an Articuno?!", "Oh no, it's that Gary guy!"]
+  @@walking_in_town_response = ["or here...", "Nope!"]
+
+  def self.welcome_to_town(user)
+    puts "==================================================="
+    puts ""
+    puts "Welcome to #{user.town_name}!"
+    puts ""
+    puts "===================================================" 
+  end
+
+  def self.found_pokemon(pokemon)
+    puts "==================================================="
+    puts ""
+    puts "YOU FOUND #{pokemon.name.upcase}!"
+    puts ""
+    puts "==================================================="
+  end
+
+  # Why does .timing have to be a class method and not simply a instance method ???
+  def self.timing
+    sleep(0.5)
+    print "."
+    sleep(0.5)
+    print "."
+    sleep(0.5)
+    print "."
+    print "\n"
+  end
+
+  def self.walking_in_town
+    legends = []
+    rand(2..4).times do 
+    phrase = @@walking_in_town.sample
+
+    puts phrase if !legends.find {|legend| phrase[legend] }
+
+    if phrase[0, 3] == "WoW"
+      legends.find { |legend| phrase[legend] } ? inIt = false : inIt = true
+    if inIt
+      legends.push(@@legendary.find { |leg| phrase[leg] })
+    end
+    elsif phrase == "No Pokemons here..."
+      timing
+      puts @@walking_in_town_response[0]
+    elsif phrase == "is that a pokemon?"
+      timing
+      puts @@walking_in_town_response[1]
+    end
+      timing
+    end
+  end   
+
+end
--- lib/models/interface.rb
+++ lib/models/interface.rb
@@ -1,128 +1,120 @@
 class Interface 
-  attr_accessor :prompt, :user, :user_current_location
-  @@trainer_pepTalk = ["Go catch them all!", "It's great to see you again."]
-  @@newbie_pepTalk = ["I hope you know what you're doing.", "Are you still there? ...chop chop... the time is ticking!", "Don't waste your time. You got work to do."]
+    attr_accessor :prompt, :user, :user_current_location
+    @@trainer_pepTalk = ["Go catch them all!", "It's great to see you again."]
+    @@newbie_pepTalk = ["I hope you know what you're doing.", "Are you still there? ...chop chop... the time is ticking!", "Don't waste your time. You got work to do."]
+    @@isItInTown = false
 
-  def initialize
-    @prompt = TTY::Prompt.new
-  end
-
-  def greet
-    puts "Welcome to the Pokemon world!"
-  end
-
-  def choose_new_trainer_or_check_inventory
-    main_menu_msg = "
-    ================================================================================
-    ==                                                                            ==
-    ==      Existing trainers: Go to your inventory and check your progress       ==
-    ==                                                                            ==
-    ==             New trainers: Start exploring the Pokemon world!               ==
-    ==                                                                            ==
-    ================================================================================
-    "
-    answer = prompt.select(main_menu_msg, ["Check Inventory", "New Trainer"])
-    trainer_info = {
-      trainer: nil,
-      isItNew: true
-    }
-    if answer == "Check Inventory"
-      current_trainer = Trainer.login_trainer
-      trainer_info[:trainer] = current_trainer
-      trainer_info[:isItNew] = false
-      trainer_info
-    elsif answer == "New Trainer"
-      new_trainer = Trainer.new_trainer
-      trainer_info[:trainer] = new_trainer
-      trainer_info
+    def initialize
+        @prompt = TTY::Prompt.new
     end
-  end
 
-  def welcome_back
-    puts "Welcome back, #{user.name}! #{@@trainer_pepTalk.sample}"
-    self.trainer_chooses_town
-    self.main_menu
-  end
-
-  def welcome_newbie
-    puts "Welcome to the game, #{user.name}! #{@@newbie_pepTalk.sample}"
-    self.trainer_chooses_pokemon
-    self.trainer_chooses_town
-    self.main_menu
-  end
-
-  def trainer_chooses_pokemon
-    poke_choices =  {"Fire" => "Charmander", "Grass" => "Bulbasaur", "Water" => "Squirtle"}
-    type_choice = prompt.select("Time to choose ", poke_choices.keys) 
-    user.pokemon = Pokemon.find_by(name: poke_choices[type_choice])
-    user.save
-  end
-
-  def trainer_chooses_town
-    town_choices = Location.where.not(town_name: "Pallet Town").map {|town| town.town_name}
-    choice = prompt.select("Please, choose your location!", town_choices) 
-    self.user_current_location = Location.find_by(town_name: choice)
-  end
-
-  def main_menu
-    # MAIN MENY GENERIC OPTIONS
-    # 1) CHECK USER'S SCORES
-    # 2) EDIT POKEMONS
-    # 3) START EXPLORING => start_exploring
-    self.start_exploring
-    puts "DISPLAY OTHER OPTIONS"
-    
-    # binding.pry
-    # self.animation
-  end
-
-  def find_wild_pokemon
-    
-    Pokemon.all.where(location_id: self.user_current_location.id).sample
-    
-  end
-
-  def start_battle
-   choice = prompt.select("WANNA FIGHT? ", ["LET'S BATTLE!", "No, I don't want to"]) 
-   if choice == "LET'S BATTLE!"
-   puts "POKEMONS ARE BATTLING "
-   elsif choice == "No, I don't want to"
-  self.no_battle
-   end 
-  end 
-
-  def no_battle 
-    battle_choice = prompt.select("Would you like to continue exploring this town?", ["Yes plz!", "No, I want to visit another town!"]) 
-    if battle_choice == "Yes plz!"
-      self.start_exploring
-    elsif battle_choice == "No, I want to visit another town!"
-      puts "GO BACK TO CHOOSE THE TOWN"
+    def greet
+        puts "Welcome to the Pokemon world!"
     end
-  end 
 
-  def yes_battle
-    #compare
-    self.find_wild_pokemon 
-    self.user.pokemon
-    #BATTLE HAPPENS
-    #Battle.new 
-  end 
+    def choose_new_trainer_or_check_inventory
+        main_menu_msg = "
+        ================================================================================
+        ==                                                                            ==
+        ==      Existing trainers: Go to your inventory and check your progress       ==
+        ==                                                                            ==
+        ==             New trainers: Start exploring the Pokemon world!               ==
+        ==                                                                            ==
+        ================================================================================
+        "
+        answer = prompt.select(main_menu_msg, ["Check Inventory", "New Trainer"])
+        trainer_info = {
+            trainer: nil,
+            isItNew: true
+        }
+        if answer == "Check Inventory"
+            current_trainer = Trainer.login_trainer
+            trainer_info[:trainer] = current_trainer
+            trainer_info[:isItNew] = false
+            trainer_info
+        elsif answer == "New Trainer"
+            new_trainer = Trainer.new_trainer
+            trainer_info[:trainer] = new_trainer
+            trainer_info
+        end
+    end
 
-  def start_exploring
-    # START EXPLORING TOWN
-    puts "==================================================="
-    puts ""
-    puts "Welcome to #{self.user_current_location.town_name}!"
-    puts ""
-    puts "==================================================="
-   sleep 3
-    # SHOW OTHER POKEMONS TO USER
-    puts "==================================================="
-    puts ""
-    puts "YOU FOUND A POKEMON #{self.find_wild_pokemon.name}!"
-    puts ""
-    puts "==================================================="
-    self.start_battle
-  end
+    def welcome_back
+        puts "Welcome back, #{user.name}! #{@@trainer_pepTalk.sample}"
+        self.trainer_chooses_town
+        self.main_menu
+    end
 
+    def welcome_newbie
+        puts "Welcome to the game, #{user.name}! #{@@newbie_pepTalk.sample}"
+        self.trainer_chooses_pokemon
+        self.trainer_chooses_town
+        self.main_menu
+    end
+
+    def trainer_chooses_pokemon
+        poke_choices =  {"Fire" => "Charmander", "Grass" => "Bulbasaur", "Water" => "Squirtle"}
+        type_choice = prompt.select("Time to choose ", poke_choices.keys) 
+        user.pokemon = Pokemon.find_by(name: poke_choices[type_choice])
+        user.save
+    end
+
+    def trainer_chooses_town
+        town_choices = Location.where.not(town_name: "Pallet Town").map {|town| town.town_name}
+        choice = prompt.select("Please, choose your location!", town_choices) 
+        self.user_current_location = Location.find_by(town_name: choice)
+    end
+
+    def main_menu
+        # MAIN MENY GENERIC OPTIONS
+        # 1) CHECK USER'S SCORES
+        # 2) EDIT POKEMONS
+        # 3) START EXPLORING => exploring_town
+        self.exploring_town
+        puts "DISPLAY OTHER OPTIONS"
+
+        # binding.pry
+        # self.animation
+    end
+
+    def find_wild_pokemon
+        Pokemon.all.where(location_id: self.user_current_location.id).sample
+    end
+
+    def start_battle
+        choice = prompt.select("Would you like to fight #{find_wild_pokemon}?", ["Yes, Let's BATTLE!", "No, I don't want to"]) 
+        if choice == "Yes, Let's BATTLE!"
+            puts "POKEMONS ARE BATTLING "
+        elsif choice == "No, I don't want to"
+            self.no_battle    
+        end 
+    end 
+
+    def no_battle 
+        options = ["Yes, I wanna keep exploring.", "No, I want to visit another town!"]
+        battle_choice = prompt.select("Would you like to continue exploring this town?", options) 
+        if battle_choice == "Yes, I wanna keep exploring."
+            self.exploring_town
+        elsif battle_choice == "No, I want to visit another town!"
+            puts "GO BACK TO CHOOSE THE TOWN"
+        end
+    end 
+
+    def yes_battle
+        #compare
+        self.find_wild_pokemon 
+        self.user.pokemon
+        #BATTLE HAPPENS
+        #Battle.new 
+    end 
+
+    def exploring_town
+        if @@isItInTown != true
+            Interactivity.welcome_to_town(user_current_location)
+            @@isItInTown = true
+        end
+        Interactivity.walking_in_town
+        Interactivity.found_pokemon(find_wild_pokemon)
+        self.start_battle
+    end
 end
