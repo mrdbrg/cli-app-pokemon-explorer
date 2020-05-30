ed90cd992bffec9368b2234b1396fdd4580df0e4
github username <github email address>
2020-05-30 15:03:58 -0400

    Work on new associations - Need additional models

--- lib/models/battle.rb
+++ lib/models/battle.rb
@@ -1,15 +1,4 @@
 class Battle < ActiveRecord::Base
   belongs_to :pokemon
   belongs_to :trainer
-
-
-
-  # ========================================================================
-  # Trainer's pokemon
-  # Battle.first.trainer.pokemons.first
-  # ========================================================================
-  # Current battle associated with the trainer and a random pokemon id
-  # Battle.first
-  # ========================================================================
 end
-
